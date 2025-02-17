5c67177ffae35080618cd8ba1ea6ce175854f7b1
Langgeng <akilnauffancozz@gmail.com>
2025-02-17 11:50:40 +0700

    initial commit config images

---
+++ src/components/modal/ModalOne.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@material-tailwind/react";
+import Image from "next/image";
+import React from "react";
+import BorderHeart from "@/assets/homepage/50503.jpg";
+import Persons from "@/assets/homepage/WhatsApp Image 2025-02-14 at 20.48.53.jpeg";
+import BorderHeart2 from "@/assets/homepage/OJZ2R60.jpg";
+
+interface PropsBorder {
+  title: string;
+  image: any;
+  desc: string;
+}
+
+const ModalOne = () => {
+  return <div className="w-full h-full bg-[#e639ba]"></div>;
+};
+
+export default ModalOne;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import ButtonComponent from "@/components/ButtonComponent/Button";
+import ButtonComponent from "@/components/buttoncomponent/Button";
+import Section1 from "@/components/section1/Section1";
 import { Button, Typography } from "@material-tailwind/react";
 import React, { ChangeEvent, useState } from "react";
 
 const Home = () => {
   return (
-    <div className="w-full flex flex-col justify-center h-full items-center">
-      <Typography className="font-poppins md:text-2xl text-sm text-green-400 font-semibold">
-        ini server kita
-      </Typography>
-      <ButtonComponent />
+    <div className="w-full flex flex-col h-full">
+      <Section1 />
     </div>
   );
 };
