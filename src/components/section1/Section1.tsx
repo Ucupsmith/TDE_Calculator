c8faeb769e5667c3c9ed78ed76bc09ecf23a1336
Langgeng <akilnauffancozz@gmail.com>
2025-05-05 07:34:33 +0700

    delete: unecessary file and folder

--- src/components/section1/Section1.tsx
+++ src/components/section1/Section1.tsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-import Navbar from "../navbar/Navbar";
-import { button, Button, Typography } from "@material-tailwind/react";
-import ModalOne from "../modal/ModalOne";
-import Image from "next/image";
-import border from "@/assets/homepage/OJZ2R60.jpg";
+import React from "react";
 
-const Section1 = () => {
-  const [buttonShow, setShowButton] = useState<boolean>(false);
-  const handleShowButton = (): void => {
-    setShowButton(!buttonShow);
-  };
-  return (
-    <div className="flex flex-col w-full max-h-min  bg-[#e639ba]">
-      <div className="w-full h-full flex justify-center items-start pt-10">
-        <div className="flex flex-col gap-3 h-full relative">
-          <Button
-            onClick={handleShowButton}
-            className="py-3 px-2 bg-[#ea50c3] w-full"
-          >
-            <Typography className="font-sans lowercase font-semibold">
-              {buttonShow ? "coba buka deh 💗 🩷" : "HAPPY VALENTINES DAY 💗"}
-            </Typography>
-          </Button>
-          {buttonShow ? null : <ModalOne />}
-          <Image src={border} alt="hehe" className="absolute w-96 top-20" />
-        </div>
-      </div>
-    </div>
-  );
+const Section1: React.FC = () => {
+  return <div>Section1</div>;
 };
 
 export default Section1;
