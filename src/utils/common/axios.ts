3f1a23feb09bcccfc55265a31446ee233bd60005
Langgeng <akilnauffancozz@gmail.com>
2025-05-31 22:09:38 +0700

    feature: setup config axios, next auth, integration tdee to home

--- src/utils/common/axios.ts
+++ src/utils/common/axios.ts
@@ -1,39 +1,48 @@
-import { getLocalStorage } from '@/utils/common/localStorage';
+import {
+  clearLocalStorage,
+  getLocalStorage
+} from '@/utils/common/localStorage';
 import type { AxiosInstance } from 'axios';
 import axios, { AxiosHeaders } from 'axios';
+import { getSession, useSession } from 'next-auth/react';
 
-const axiosInterceptor = (
-  url: string,
-  isAuthenticated: boolean = false,
-  additionalHeaders = {}
-): AxiosInstance => {
-  const customHeader = AxiosHeaders.from(additionalHeaders);
-
+const axiosInterceptor = (url: string): AxiosInstance => {
   const axiosCreate = axios.create({
     baseURL: url,
     headers: {
       Platform: 'web',
       Accept: 'application/json',
-      'Accept-Language': 'es',
-      'Content-Type': 'application/json',
-      ...(isAuthenticated
-        ? { Authorization: `Bearer ${getLocalStorage('accessToken', '')}` }
-        : null),
-      ...customHeader
+      'Accept-Language': 'id',
+      'Content-Type': 'application/json'
     }
   });
 
-  axiosCreate.interceptors.response.use(
-    (response) => {
-      return response.data;
-    },
-    async (error) => {
-      if (error.response.status === 401) {
-        // clear all auth cookie or auth local storage
+  // Interceptor untuk menambahkan token dari NextAuth
+  axiosCreate.interceptors.request.use(
+    async (config) => {
+      const session = await getSession();
+
+      if (session?.user.accessToken) {
+        config.headers.Authorization = `Bearer ${session.user.accessToken}`;
       }
-      return await Promise.reject(error);
-    }
+
+      return config;
+    },
+    (error) => Promise.reject(error)
   );
+
+  // axiosCreate.interceptors.response.use(
+  //   (response) => {
+  //     return response.data;
+  //   },
+  //   async (error) => {
+  //     if (error.response.status === 401) {
+  //       clearLocalStorage();
+  //       // clear all auth cookie or auth local storage
+  //     }
+  //     return await Promise.reject(error);
+  //   }
+  // );
 
   return axiosCreate;
 };
